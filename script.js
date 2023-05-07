f97c29c4e577237162b8d026528daf806e78f982
WellinsonNunes <wellbite01@gmail.com>
2023-05-07 16:31:40 -0300

    site

---
+++ script.js
@@ -0,0 +1,51 @@
+var button = document.querySelector('#executar')
+var letra = document.querySelector('#letra1')
+
+button.addEventListener('click', function(){
+    if(letra1.style.display === 'block') {
+        letra1.style.display = 'none';
+    } else {
+        letra1.style.display = 'block';
+    }
+
+
+})
+
+var button2 = document.querySelector('#executar2')
+var letra2 = document.querySelector('#letra2')
+
+button2.addEventListener('click', function(){
+    if(letra2.style.display === 'block') {
+        letra2.style.display = 'none';
+    } else {
+        letra2.style.display = 'block';
+    }
+
+
+})
+
+var button = document.querySelector('#executar')
+var letra = document.querySelector('.letra')
+
+button.addEventListener('click', function(){
+    if(letra.style.display === 'block') {
+        letra.style.display = 'none';
+    } else {
+        letra.style.display = 'block';
+    }
+
+
+})
+
+var button = document.querySelector('#executar')
+var letra = document.querySelector('.letra')
+
+button.addEventListener('click', function(){
+    if(letra.style.display === 'block') {
+        letra.style.display = 'none';
+    } else {
+        letra.style.display = 'block';
+    }
+
+
+})
